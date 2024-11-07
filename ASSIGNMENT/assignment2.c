5d0e770017df1af4394cfd5006a79837373bdc03
indhu A <induananda123@gmail.comgit config --global user.email induananda123@gmail.comgit config --global user.name indhu.a>
2024-11-07 19:22:22 +0530

    assignment

---
+++ ASSIGNMENT/assignment2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int power(int n1, int n2);
+int main() {
+    int base, exp, result;
+    printf("Enter base number=: ");
+    scanf("%d", &base);
+    printf("Enter exponensial number=");
+    scanf("%d",&exp")
+
+    
